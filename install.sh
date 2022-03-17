4500c0c862af40a151f9db4820a6cbab5b2e875a
Arthur Augusto Magalhães <kitsune@pop-os.localdomain>
2022-03-17 18:48:14 -0300

    remake install for all linux system

--- install.sh
+++ install.sh
@@ -1,61 +1,51 @@
-#!/bin/bash
+#!/bin/env bash
 
-#update system 
-sudo apt update ; 
-sudo apt upgrade -y ;
-#install dependencies
-sudo apt install build-essential default-jdk libssl-dev exuberant-ctags -y && 
-sudo apt install ncurses-term ack-grep silversearcher-ag fontconfig -y &&
-sudo apt install imagemagick libmagickwand-dev software-properties-common git curl -y &&
-sudo apt install python3 python3-pip python3-venv &&
-
-
-#configuring fonts 
-sudo cp -r ./fonts/* /usr/local/share/fonts/ &&
-fc-cache -fv &&
-
-# configuring /home 
-mkdir ~/Projetos/ &&
-touch ~/.aliasrc &&
-cp ./aliasrc ~/.aliasrc &&
-cat /dev/null > ~/.bashrc &&
-cp ./bashrc ~/.bashrc &&
-
-### install asdf
-
+#----------------------------- Install Asdf --------------------------------------------------------
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
-
-# install asdf langs
-
+printf "Asdf Instalado com Sucesso \n" &&
+#----------------------------- Configuring fonts ---------------------------------------------------
+sudo cp -r ./assets/fonts/* /usr/local/share/fonts/ &&
+fc-cache -fv &&
+printf "Fonts FiraCode com NerdFonts instalados com sucesso \n" && 
+#---------------------------- Configuring Bashrc template ------------------------------------------
+cat /dev/null > ~/.bashrc &&
+cp ./template/bashrc ~/.bashrc &&
+touch ~/.aliasrc &&
+cp ./template/aliasrc ~/.aliasrc &&
+printf "Templates for bashrc and aliasrc configurados com sucesso \n" &&
+# -------------------------- Install Asdf Languages ------------------------------------------------
+asdf plugin add python ;
+asdf plugin add golang ;
+asdf plugin add ruby ;
+asdf plugin add rust ;
+asdf plugin add lua ;
 asdf plugin add nodejs ;
+
+asdf install python 3.10.2 &&
+asdf install golang 1.17.7 &&
+asdf install ruby 3.1.1 &&
+asdf install rust 1.58.1 &&
+asdf install lua 5.4.4 &&
 asdf install nodejs 17.5.0 &&
+
+asdf global python 3.10.2 &&
+asdf global golang 1.17.7 &&
+asdf global ruby 3.1.1 &&
+asdf global rust 1.58.1 &&
+asdf global lua 5.4.4 &&
 asdf global nodejs 17.5.0 &&
 
-asdf plugin add golang ; 
-asdf install golang 1.17.7 &&
-asdf global golang 1.17.7 &&
+printf "Linguagens instaladas com sucesso \n" &&
 
-asdf plugin add lua ;
-asdf install lua 5.4.4 &&
-asdf global lua 5.4.4 &&
-
-asdf plugin add ruby ;
-asdf install ruby 3.1.1 &&
-asdf global ruby 3.1.1 &&
-
-asdf plugin add rust ;
-asdf install rust 1.58.1 &&
-asdf global rust 1.58.1 &&
-
-asdf plugin install python;
-asdf install python 3.10.2 &&
-asdf global python 3.10.2 &&
-
-# install program writed with rust
-
-cargo install bat exa duf fd;
-
-# install lunarvim
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y ; 
-source ~/.bashrc &&
-
+# ----------------------- Install Lunarvim ---------------------------------------------------------
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y &&
+printf "Lunarvim instalado com sucesso \n"
+# ----------------------- Install Rust Tools -------------------------------------------------------
+cargo install bat exa &&
+printf "Ferramentas em rust instaladas com sucesso \n"
+# ----------------------- Criando aplicação asdf-reload --------------------------------------------
+sudo cp ./src/asdf-reload /usr/bin/ &&
+printf "Asdf-reload instalado \n"
+# ----------------------- Reload bashrc ------------------------------------------------------------
+clear &&
+source ~/.bashrc
