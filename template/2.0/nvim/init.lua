54dc58a25d9da2b08fa955f3675ea73934aa96b6
KitsuneSemCalda <59454155+KitsuneSemCalda@users.noreply.github.com>
2022-06-12 11:26:33 -0300

    Delete template/2.0/nvim directory

--- template/2.0/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env lua
-
-require("configs.settings")
-require("configs.mapping")
-require("plugins.plugins")
-require("plugins.complete")
-
--- template/2.0/nvim/lua/plugins/complete.lua
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env lua
-
-if extension == "md" then
-  require("plugins.tabnine")
-else
-
-  require("plugins.lsp")
-end
